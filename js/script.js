8b1712fffca977257745b0747d6f0a2e01922b08
SquallStrifeX <squallstrifex@gmail.com>
2023-12-11 16:44:59 +0100

    end exercise

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ let { createApp } = Vue
 createApp({
   data() {
     return {
-      message: "Meglio soli che da soli in due"
+      message: "Meglio soli che da soli in due",
+      image:'https://new-game-plus.fr/wp-content/uploads/2022/01/Final-Fantasy-VII-Remake-Intergrade-PC.jpg.webp'
+   
     }
   }
 }).mount('#app')
